b1a72e7a4d88415ff385b29d8988997ae073110d
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-04-09 15:56:33 -0600

    Added functionality to mark completed TODOs and delete TODOs

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -16,28 +16,58 @@ const defaultElements = [
 
 function TodoContainer() {
   const [todoElements, setTodoElements] = React.useState(defaultElements)
+
   const [searchValue, setSearchValue] = React.useState('')
+
   const todoCompleted = todoElements.filter(todo => todo.completed).length
+
   const filterTodos = todoElements.filter((element) => {
     let todoDescription = element.text.toLowerCase()
     let searchText = searchValue.toLowerCase()
     return todoDescription.includes(searchText)
   })
-  
+
+  const selectVerification = (key) => {
+    let newTodos = [...todoElements]
+    let index = newTodos.findIndex((element) => element.text === key)
+    newTodos[index].completed ? newTodos[index].completed = false : newTodos[index].completed = true
+    setTodoElements(newTodos)
+  }
+
+  const todoDelete = (key) => {
+    let newTodos = [...todoElements]
+    let index = newTodos.findIndex((element) => element.text === key)
+    newTodos.splice(index, 1) // delete elements from parameter 1 to prameter 2
+    setTodoElements(newTodos)
+  }
+
   return React.createElement(
     'div',
     {className : 'todoContainer'},
     <React.Fragment>
-      <TodoCounter finishNumber={todoCompleted} totalTodoThings={todoElements.length} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
+      <TodoCounter 
+        finishNumber={todoCompleted} 
+        totalTodoThings={todoElements.length} 
+      />
+      <TodoSearch 
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
         { 
-          filterTodos.map((
-              (todo) => <TodoItem key={todo.text} todo={todo} />
+          filterTodos.map(((todo) => 
+              <TodoItem 
+                key={todo.text} todo={todo} 
+                onComplete={() => selectVerification(todo.text)}
+                onDelete={() => todoDelete(todo.text)}
+              />
             ))
         }
       </TodoList>
-      <TodoCreateButton type={"+"} action={() => alert('Creando...')} />
+      <TodoCreateButton 
+        type={"+"} 
+        action={() => alert('Creando...')} 
+      />
     </React.Fragment>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,15 @@
 import '../assets/styles/TodoItem.css'
 
-function TodoItem({ todo }) {
-  console.log(todo)
+function TodoItem(props) {
   return (
     <li className="todoItem">
-      <span className={'icon-verification ' + (todo.completed ? 'complete-color' : '') }>V</span>
-      <p className={todo.completed ? 'complete-mark' : ''}>
-        {todo.text}
+      <span className={'icon-verification ' + (props.todo.completed ? 'complete-color' : '')} onClick={props.onComplete}>
+        V
+      </span>
+      <p className={props.todo.completed ? 'complete-mark' : ''}>
+        {props.todo.text}
       </p>
-      <span className='icon-cancel'> X </span>
+      <span className='icon-cancel' onClick={props.onDelete}> X </span>
     </li>
   );
 }
