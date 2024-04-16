6f9c53a7faa646d699bfcad1f620172992b0c2e6
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-04-16 12:24:59 -0600

    Added styles to adapt container APP in different aspect relations

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -21,7 +21,6 @@ function TodoContainer() {
     'div',
     {className : 'todoContainer'},
     <React.Fragment>
-      {/* Modals */}
       <TodoMessage />
       <TodoCounter />
       <TodoSearch />
--- src/pages/todoMachine/App.jsx
+++ src/pages/todoMachine/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { TodoContainer } from '../../components/TodoContainer'
 import { TodoProvider } from '../../contexts/TodoContext'
 
-
+import '../../assets/styles/TodoMachine.css'
 
 function App() {
-  return (
+  return React.createElement(
+    'div',
+    {className: 'todoMachineWarapper'},
     <TodoProvider>
       <TodoContainer/>
     </TodoProvider>
